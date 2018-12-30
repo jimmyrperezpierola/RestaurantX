3158d9a68bb3cee1f82fbe93fef13c0ba14b5855
aanfaisal <aan.faisalal@gmail.com>
2018-12-30 09:53:53 +0700

    del unused declaration :v

--- src/views/screens/main/index.tsx
+++ src/views/screens/main/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import { Image, Platform, StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
 import { MainScreenNav } from './nav'
 import { MainScreenProps } from './props'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-})
 
 export class MainScreen extends React.Component<MainScreenProps> {
   constructor(props: MainScreenProps) {
@@ -24,21 +18,4 @@ export class MainScreen extends React.Component<MainScreenProps> {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
+
